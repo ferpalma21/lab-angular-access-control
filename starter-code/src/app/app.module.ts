0f4621768221602fefdb4a8a9e912822b5733625
Fernando Palma <ferpalma21@github.com>
2017-07-27 09:40:07 +0200

    starting 1st iteration

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AccessControlLog } from '../service/AccessControlLog.service';
+
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +16,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AccessControlLog],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
